f2a17960a82db6bd1c656eba00b24de71487c93c
김나경 <recodepan1219@egmail.com>
2023-07-17 02:09:44 +0900

    헤더 만들기

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import Search from '@/assets/svg/search'
 
 const styles = {
   header: 'bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]',
-
+  headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+  nav: `flex justify-center items-center gap-[20px]`,
+  navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
+  badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
+  inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
+  input: `bg-transparent outline-none text-white w-70 ml-3`,
 }
 
 const Header = () => {
@@ -12,9 +18,59 @@ const Header = () => {
       <Image
       src='/logo.png' 
       alt='logo'
-      width={220}
-      height={220}
+      width={220} //가로
+      height={220} //세로
       />
+
+      <div className={styles.headerWrapper}>
+        <nav className={styles.nav}>
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Cryptocurrencies</div>
+            <div className={styles.badge} />
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Exchanges</div>
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>NFT</div>
+            <div className={styles.badge} />
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Cryptown</div>
+            <div className={styles.badge} />
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Portfolio</div>
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Watchlist</div>
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Products</div>
+            <div className={styles.badge} />
+          </div>
+
+          <div className={styles.navItem}>
+            <div className={styles.navLink}>Learn</div>
+          </div>
+        </nav>
+
+        <div className='flex items-center'>
+          {/* <ConnectButton /> */}
+
+          <div className={styles.inputContainer}>
+            <Search />
+            <input className={styles.input} placeholder='Search' />
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
