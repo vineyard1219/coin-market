bf57f95d25cf8f5c36c248831c706de9c99daeca
김나경 <recodepan1219@egmail.com>
2023-07-20 20:06:46 +0900

    존나 얼레벌레 중간 저장

--- components/cmc-table/CMCTable.tsx
+++ components/cmc-table/CMCTable.tsx
@@ -2,7 +2,8 @@
 
 import React, { useContext, useEffect, useState, useCallback } from 'react'
 import { CoinMarketContext } from '../../context/context'
-import CMCTableHeader from './CMCtableHeader'
+import CMCTableHeader from './CMCTableHeader'
+import CMCTableRow from './CMCTableRow'
 
 interface ICoin {
   cmc_rank: number
@@ -19,7 +20,7 @@ const CMCTable: React.FC = () => {
 
   const setData = useCallback(async () => {
     try {
-      const apiResponse: ICoin[] = await getTopTenCoins()
+      const apiResponse: ICoin[] = await getTopTenCoins() // getTopTenCoins type is assumed to be correctly inferred
       const filteredResponse: ICoin[] = []
 
       for (let i = 0; i < apiResponse.length; i++) {
@@ -34,34 +35,29 @@ const CMCTable: React.FC = () => {
   }, [getTopTenCoins])
 
   return (
-    <div className= 'test-white font-bold'>
+    <div className='text-white font-bold'>
       <div className='mx-auto max-w-screen-2xl'>
         <table className='w-full'>
-          <CMCTableHeader/>
-
-          {coinData && coinData ? (
-            coinData.map((coin,index)=> {
-              // return(
-              //   // <CMCTableRow
-              //   // key = {index}
-              //   // starNum = {coin.cmc_rank}
-              //   // coinName = {coin.name}
-              //   // coinSymbol = {btc}
-              //   // showBuy = {true}
-              //   // hRate = {coin.quote.USD.percent_change_24H}
-              //   // dRate = {coin.quote.USD.percent_change_7d}
-              //   // hRateIsIncrement = {true}
-              //   // price = {coin.quote.USD.price}
-              //   // marketCapValue = {coin.quote.USD.market_cap}
-              //   // volumeCryptoValue = {coin.quote.USD.volume_24h}
-              //   // volumeValue={coin.total_supply}
-              //   // circulatingSupply={coin.circulating_supply}               
-              //   // />
-              // )
-            })
-          ) :(
-            <></>
-          )}
+          <CMCTableHeader />
+          <CMCTableRow />
+          {coinData && coinData.map((coin, index) => (
+            <CMCTableRow
+              key={index}
+              starNum={coin.cmc_rank}
+              coinName={coin.name}
+              coinSymbol={coin.symbol}
+              coinIcon={btc}
+              showBuy={true}
+              hRate={coin.quote.USD.percent_change_24h}
+              dRate={coin.quote.USD.percent_change_7d}
+              hRateIsIncrement={true}
+              price={coin.quote.USD.price}
+              marketCapValue={coin.quote.USD.market_cap}
+              volumeCryptoValue={coin.quote.USD.volume_24h}
+              volumeValue={coin.total_supply}
+              circulatingSupply={coin.circulating_supply}
+            />
+          ))}
         </table>
       </div>
     </div>
